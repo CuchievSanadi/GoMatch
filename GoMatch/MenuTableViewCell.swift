b9f61020851a304c6a32e5c5ec9ef4cb846773f4
CuchievSanadi <sanadicuchiev@MacBook-Pro-Sanadi.local>
2016-01-29 21:13:44 +0300

    feat(base): change stor

---
+++ GoMatch/MenuTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuTableViewCell.swift
+//  GoMatch
+//
+//  Created by Sanadi Cuchiev on 29.01.16.
+//  Copyright © 2016 Sanadi Cuchiev. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
